297b949fd30eeebddb74baba220f6323fef38573
plotavio <pltavio@gmail.com>
2022-10-28 13:56:05 -0300

    CORS

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,11 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import {Cors} from 'Cors'
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
+
 
   @Get()
   getHello(): string {
